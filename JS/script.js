fbe993016724bcae164d68a6f9db6dc75d23e443
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-10 15:48:06 +0200

    corrected the brackets

--- JS/script.js
+++ JS/script.js
@@ -9,12 +9,18 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 //stabilisco le condizioni del ciclo
 for (let i = 1; i <= 100; i++){
     //scrivo le variabili a cascata
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log('FizzBuzz');
-    } else if(i != 3 && i % 3 === 0){
+    if ((i % 3 === 0) && (i % 5 === 0)){
+
+        console.log("FizzBuzz");
+
+    } else if ((i != 3) && (i % 3 === 0)){
+
         console.log("Fizz")
-    } else if(i != 5 && i % 5 === 0){
+
+    } else if ((i != 5) && (i % 5 === 0)){
+
         console.log("Buzz")
+
     } else {
         console.log(i);
     }
