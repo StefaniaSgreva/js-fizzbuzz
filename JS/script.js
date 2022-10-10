59528a60003f1dcc59f589f67b5a0d0d0f4947b5
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-10 15:22:30 +0200

    added != 3 and 5

--- JS/script.js
+++ JS/script.js
@@ -11,9 +11,9 @@ for (let i = 1; i <= 100; i++){
     //scrivo le variabili a cascata
     if(i % 3 === 0 && i % 5 === 0){
         console.log('FizzBuzz');
-    } else if(i % 3 === 0){
+    } else if(i != 3 && i % 3 === 0){
         console.log("Fizz")
-    } else if(i % 5 === 0){
+    } else if(i != 5 && i % 5 === 0){
         console.log("Buzz")
     } else {
         console.log(i);
